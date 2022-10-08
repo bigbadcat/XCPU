813cc5385927628e0f0256170a8b8bc737fe0477
bigbadcat <xuxiang_study@163.com>
2022-10-08 14:00:46 +0800

    取指令操作

--- XKit/XKit/MicroProgram.cs
+++ XKit/XKit/MicroProgram.cs
@@ -14,9 +14,9 @@ namespace XKit
     public class MicroProgram
     {
         /// <summary>
-        /// 微程序容量。(数据宽度4个字节，8个地址线)
+        /// 微程序容量。(数据宽度4个字节，16个地址线)
         /// </summary>
-        public const int MICRO_SIZE = 4 * (1 << 8);
+        public const int MICRO_SIZE = 4 * (1 << 16);
 
         /// <summary>
         /// 微程序缓冲区。
@@ -25,40 +25,88 @@ namespace XKit
 
         #region 引脚定义----------------------------------------------------------------
 
-        public const int PIN_D_CS = 1 << 0;
-        public const int PIN_D_W = 1 << 1;
-        public const int PIN_C_CS = 1 << 2;
-        public const int PIN_C_W = 1 << 3;
+        public const int SHIFT_REG_R = 0;       //寄存器读偏移量
+        public const int SHIFT_REG_W = 5;       //寄存器写偏移量
+        public const int A_R = Pin.REG_A << SHIFT_REG_R;
+        public const int B_R = Pin.REG_B << SHIFT_REG_R;
+        public const int C_R = Pin.REG_C << SHIFT_REG_R;
+        public const int D_R = Pin.REG_D << SHIFT_REG_R;
+        public const int CS_R = Pin.REG_CS << SHIFT_REG_R;
+        public const int DS_R = Pin.REG_DS << SHIFT_REG_R;
+        public const int SS_R = Pin.REG_SS << SHIFT_REG_R;
+        public const int SP_R = Pin.REG_SP << SHIFT_REG_R;
+        public const int MSR_R = Pin.REG_MSR << SHIFT_REG_R;
+        public const int MAR_R = Pin.REG_MAR << SHIFT_REG_R;
+        public const int SRC_R = Pin.REG_SRC << SHIFT_REG_R;
+        public const int DST_R = Pin.REG_DST << SHIFT_REG_R;
+        public const int IRL_R = Pin.REG_IRL << SHIFT_REG_R;
+        public const int IRH_R = Pin.REG_IRH << SHIFT_REG_R;
+        public const int A_W = Pin.REG_A << SHIFT_REG_W;
+        public const int B_W = Pin.REG_B << SHIFT_REG_W;
+        public const int C_W = Pin.REG_C << SHIFT_REG_W;
+        public const int D_W = Pin.REG_D << SHIFT_REG_W;
+        public const int CS_W = Pin.REG_CS << SHIFT_REG_W;
+        public const int DS_W = Pin.REG_DS << SHIFT_REG_W;
+        public const int SS_W = Pin.REG_SS << SHIFT_REG_W;
+        public const int SP_W = Pin.REG_SP << SHIFT_REG_W;
+        public const int MSR_W = Pin.REG_MSR << SHIFT_REG_W;
+        public const int MAR_W = Pin.REG_MAR << SHIFT_REG_W;
+        public const int SRC_W = Pin.REG_SRC << SHIFT_REG_W;
+        public const int DST_W = Pin.REG_DST << SHIFT_REG_W;
+        public const int IRL_W = Pin.REG_IRL << SHIFT_REG_W;
+        public const int IRH_W = Pin.REG_IRH << SHIFT_REG_W;
 
-        public const int PIN_OP_SHIFT = 4;
-        public const int PIN_OP_ADD = 1 << PIN_OP_SHIFT;
-        public const int PIN_OP_SUB = 2 << PIN_OP_SHIFT;
-        public const int PIN_OP_MUL = 3 << PIN_OP_SHIFT;
-        public const int PIN_OP_IMUL = 4 << PIN_OP_SHIFT;
-        public const int PIN_OP_DIV = 5 << PIN_OP_SHIFT;
-        public const int PIN_OP_IDIV = 6 << PIN_OP_SHIFT;
-        public const int PIN_OP_MOD = 7 << PIN_OP_SHIFT;
-        public const int PIN_OP_IMOD = 8 << PIN_OP_SHIFT;
-        public const int PIN_OP_AND = 9 << PIN_OP_SHIFT;
-        public const int PIN_OP_OR = 10 << PIN_OP_SHIFT;
-        public const int PIN_OP_NOT = 11 << PIN_OP_SHIFT;
-        public const int PIN_OP_XOR = 12 << PIN_OP_SHIFT;
-        public const int PIN_OP_CMP = 13 << PIN_OP_SHIFT;
-        public const int PIN_OP_ICMP = 14 << PIN_OP_SHIFT;
-        public const int PIN_OP_PSET = 15 << PIN_OP_SHIFT;
+        public const int I_SRC_R = 1 << 10;         //指令源寄存器读
+        public const int I_SRC_W = 3 << 10;         //指令源寄存器写
+        public const int I_DST_R = 1 << 12;         //指令目标寄存器读
+        public const int I_DST_W = 3 << 12;         //指令目标寄存器写
 
-        public const int PIN_B_CS = 1 << 8;
-        public const int PIN_B_W = 1 << 9;
-        public const int PIN_A_CS = 1 << 10;
-        public const int PIN_A_W = 1 << 11;
-        public const int PIN_PC_EN = 1 << 12;
-        public const int PIN_PC_W = 1 << 13;
-        public const int PIN_PC_CS = 1 << 14;
-        public const int PIN_MAR_CS = 1 << 15;
-        public const int PIN_MAR_W = 1 << 16;
-        public const int PIN_MC_CS = 1 << 17;
-        public const int PIN_MC_W = 1 << 18;
-        public const int PIN_HALT = 1 << 30;
+        public const int PC_R = 1 << 14;            //PC读
+        public const int PC_W = 3 << 14;            //PC写
+        public const int PC_INC = 7 << 14;          //PC自增
+
+        public const int SHIFT_OP = 17;                         //操作偏移量
+        public const int OP_ADD = Pin.OP_ADD << SHIFT_OP;       //加法
+        public const int OP_SUB = Pin.OP_SUB << SHIFT_OP;       //减法
+        public const int OP_MUL = Pin.OP_MUL << SHIFT_OP;       //无符号乘法
+        public const int OP_IMUL = Pin.OP_IMUL << SHIFT_OP;     //有符号乘法
+        public const int OP_DIV = Pin.OP_DIV << SHIFT_OP;       //无符号除法
+        public const int OP_IDIV = Pin.OP_IDIV << SHIFT_OP;     //有符号除法
+        public const int OP_MOD = Pin.OP_MOD << SHIFT_OP;       //无符号取余
+        public const int OP_IMOD = Pin.OP_IMOD << SHIFT_OP;     //有符号取余
+        public const int OP_AND = Pin.OP_AND << SHIFT_OP;       //与
+        public const int OP_OR = Pin.OP_OR << SHIFT_OP;         //或
+        public const int OP_NOT = Pin.OP_NOT << SHIFT_OP;       //非
+        public const int OP_XOR = Pin.OP_XOR << SHIFT_OP;       //异或
+        public const int OP_CMP = Pin.OP_CMP << SHIFT_OP;       //无符号比较
+        public const int OP_ICMP = Pin.OP_ICMP << SHIFT_OP;     //有符号比较
+        public const int OP_PSET = Pin.OP_PSET << SHIFT_OP;     //状态字设置
+
+        public const int MC_R = 1 << 21;            //内存读
+        public const int MC_W = 3 << 21;            //内存写
+
+        public const int CYC_RS = 1 << 29;          //重置微指令周期
+        public const int HALT = 1 << 30;            //停止
+
+        #endregion
+
+        #region 指令定义----------------------------------------------------------------
+
+        /// <summary>
+        /// 取指。
+        /// </summary>
+        public static int[] Fetch = new int[]
+        {
+            CS_R | MSR_W,
+            PC_R | MAR_W,
+            MC_R | IRH_W | PC_INC,
+            PC_R | MAR_W,
+            MC_R | IRL_W | PC_INC,
+            PC_R | MAR_W,
+            MC_R | SRC_W | PC_INC,
+            PC_R | MAR_W,
+            MC_R | DST_W | PC_INC,
+        };
 
         #endregion
 
@@ -69,35 +117,87 @@ namespace XKit
         /// <param name="file">保存微程序的二进制文件。</param>
         public static void Build(string file)
         {
-            int[] pin_data = new int[]
+            int addr = MicroBuffer.Length >> 2;
+            for (int i=0; i< addr; ++i)
             {
-                PIN_PC_CS | PIN_MAR_W | PIN_MAR_CS,
-                PIN_MC_CS | PIN_A_W | PIN_A_CS,
-                PIN_PC_W | PIN_PC_CS | PIN_PC_EN,
-                PIN_PC_CS | PIN_MAR_W | PIN_MAR_CS,
-                PIN_MC_CS | PIN_B_W | PIN_B_CS,
-                PIN_OP_SUB | PIN_C_W | PIN_C_CS,
-                PIN_OP_DIV | PIN_D_W | PIN_D_CS,
-                PIN_PC_W | PIN_PC_CS | PIN_PC_EN,
-                PIN_PC_CS | PIN_MAR_W | PIN_MAR_CS,
-                PIN_MC_W | PIN_MC_CS | PIN_C_CS,
-                PIN_PC_W | PIN_PC_CS | PIN_PC_EN,
-                PIN_PC_CS | PIN_MAR_W | PIN_MAR_CS,
-                PIN_MC_W | PIN_MC_CS | PIN_D_CS,
-            };
+                int cyc = i & 0xF;      //后四位表示微指令周期
+                if (cyc < Fetch.Length)
+                {
+                    WriteUInt32(MicroBuffer, (uint)(i << 2), (uint)Fetch[cyc]);
+                    continue;
+                }
 
-            for (int i=0;i<pin_data.Length; ++i)
-            {
-                WriteUInt32(MicroBuffer, (uint)(i << 2), (uint)pin_data[i]);
+                int instruct = i >> 2;
+                int mop = HALT;
+                cyc -= Fetch.Length;
+                if ((instruct & 0x8000) != 0)
+                {
+                    //双操作数
+                    int src = instruct & 0x3;
+                    int dst = (instruct >> 2) & 0x3;
+                    int cmd = (instruct >> 4) & 0x7F;
+                    mop = GetMicroInstruct2(cyc, cmd, dst, src);
+
+                }
+                else if ((instruct & 0x4000) != 0)
+                {
+                    //单操作数
+                    int psw = instruct & 0x7;
+                    int dst = (instruct >> 3) & 0x3;
+                    int cmd = (instruct >> 7) & 0x1F;
+                    mop = GetMicroInstruct1(cyc, cmd, dst, psw);
+                }
+                else
+                {
+                    //无操作数
+                    int psw = instruct & 0x7;
+                    int cmd = (instruct >> 3) & 0x7F;
+                    mop = GetMicroInstruct0(cyc, cmd, psw);
+                }
+                WriteUInt32(MicroBuffer, (uint)(i << 2), (uint)mop);
             }
-            int addr = 1 << 8;
-            for (int i = pin_data.Length; i < addr; ++i)
-            {
-                WriteUInt32(MicroBuffer, (uint)(i << 2), (uint)PIN_HALT);
-            }
+
 
             File.WriteAllBytes(file, MicroBuffer);
             MessageBox.Show("Build micro program finished.");
+        }
+
+        /// <summary>
+        /// 获取双操作数的指令。
+        /// </summary>
+        /// <param name="cyc">微指令周期。</param>
+        /// <param name="cmd"></param>
+        /// <param name="dst"></param>
+        /// <param name="src"></param>
+        /// <returns></returns>
+        public static int GetMicroInstruct2(int cyc, int cmd, int dst, int src)
+        {
+            return HALT;
+        }
+
+        /// <summary>
+        /// 获取单操作数的指令。
+        /// </summary>
+        /// <param name="cyc">微指令周期。</param>
+        /// <param name="cmd"></param>
+        /// <param name="dst"></param>
+        /// <param name="psw"></param>
+        /// <returns></returns>
+        public static int GetMicroInstruct1(int cyc, int cmd, int dst, int psw)
+        {
+            return HALT;
+        }
+
+        /// <summary>
+        /// 获取无操作数的指令。
+        /// </summary>
+        /// <param name="cyc">微指令周期。</param>
+        /// <param name="cmd"></param>
+        /// <param name="psw"></param>
+        /// <returns></returns>
+        public static int GetMicroInstruct0(int cyc, int cmd, int psw)
+        {
+            return HALT;
         }
 
         /// <summary>
---
+++ XKit/XKit/Pin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XKit
+{
+    /// <summary>
+    /// 针脚定义。
+    /// </summary>
+    public class Pin
+    {
+        public const int REG_A = 0B00001;       //通用A
+        public const int REG_B = 0B00010;       //通用B
+        public const int REG_C = 0B00011;       //通用C
+        public const int REG_D = 0B00100;       //通用D
+        public const int REG_CS = 0B00101;      //代码段
+        public const int REG_DS = 0B00110;      //数据段
+        public const int REG_SS = 0B00111;      //堆栈段
+        public const int REG_SP = 0B01000;      //堆栈指针
+        public const int REG_MSR = 0B01001;     //内存段寄存器
+        public const int REG_MAR = 0B01010;     //内存地址寄存器
+        public const int REG_SRC = 0B01011;     //源操作数寄存器
+        public const int REG_DST = 0B01100;     //目的操作数寄存器
+        public const int REG_IRL = 0B01101;     //指令寄存器低8位
+        public const int REG_IRH = 0B01110;     //指令寄存器高8位
+
+        public const int OP_ADD = 0B0001;       //加法
+        public const int OP_SUB = 0B0010;       //减法
+        public const int OP_MUL = 0B0011;       //无符号乘法
+        public const int OP_IMUL = 0B0100;      //有符号乘法
+        public const int OP_DIV = 0B0101;       //无符号除法
+        public const int OP_IDIV = 0B0110;      //有符号除法
+        public const int OP_MOD = 0B0111;       //无符号取余
+        public const int OP_IMOD = 0B1000;      //有符号取余
+        public const int OP_AND = 0B1001;       //与
+        public const int OP_OR = 0B1010;        //或
+        public const int OP_NOT = 0B1011;       //非
+        public const int OP_XOR = 0B1100;       //异或
+        public const int OP_CMP = 0B1101;       //无符号比较
+        public const int OP_ICMP = 0B1110;      //有符号比较
+        public const int OP_PSET = 0B1111;      //状态字设置
+    }
+}
